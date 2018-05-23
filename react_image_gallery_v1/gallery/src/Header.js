7d951fcf509c3dbcda7d12ef2214d410b137e4bd
Cyndi Norwood <cyndinorwood@me.com>
2018-05-23 13:51:32 -0700

    fixed header

--- react_image_gallery_v1/gallery/src/Header.js
+++ react_image_gallery_v1/gallery/src/Header.js
@@ -1,11 +1,15 @@
 
 import React, { Component } from "react";
+import Nav from './Nav';
+import SearchForm from './SearchForm';
+
 class Header extends Component {
   render() {
     return (
       <div className="container">
         <h2>Flickr Search</h2>
-
+          <Nav />
+          <SearchForm />
       </div>
     );
   }
