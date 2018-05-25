929b7ab2216482c5d56d441003f91d417821ab5e
Cyndi Norwood <cyndinorwood@me.com>
2018-05-25 10:04:30 -0700

    Fixed the Search form not showing up on the button pages
    added it to the header instead

--- react_image_gallery_v1/gallery/src/Header.js
+++ react_image_gallery_v1/gallery/src/Header.js
@@ -1,13 +1,13 @@
 
 import React, { Component } from "react";
 import SearchForm from './SearchForm';
-
+import apiKey from './Config';
 class Header extends Component {
   render() {
     return (
       <div className="container">
         <h2>Flickr Search</h2>
-<SearchForm/>
+<SearchForm api={apiKey}/>
       </div>
     );
   }
